ec94a9c8e90be4ab85432ed17a6ca11fc48c547a
Cameron Ornelas <35507124+CameronOrnelas@users.noreply.github.com>
2018-05-03 19:20:21 -0500

    Second Commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,8 @@
+'use strict'
+const printListOfOlympians = (array)=>{
+  return array.forEach(function(item){
+    console.log(item);
+});
+}
+
+printListOfOlympians(['Michael Phelps','Larisa Latynina','Paavo Nurmi','Mark Spitz','Carl Lewis'])
