33cddb991a89003e3abdd5f074b325f76c3d93e9
Cameron Ornelas <35507124+CameronOrnelas@users.noreply.github.com>
2018-05-10 22:14:52 -0500

    First Exercise

--- 04week/loop.js
+++ 04week/loop.js
@@ -1,1 +1,8 @@
 'use strict'
+
+//For Loop Used to return all items in an array
+const carsInReverse = ['Ford','Dodge','BMW','Cadillac'];
+const arrLength = carsInReverse.length;
+for(i = 0; i < arrLength; i++){
+  console.log (carsInReverse[i]);
+};
