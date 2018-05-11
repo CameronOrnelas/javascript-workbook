4758a918abf8e6d1c978b673a05aee74bd8ac1c3
Cameron Ornelas <35507124+CameronOrnelas@users.noreply.github.com>
2018-05-10 23:08:06 -0500

    Second Exercise

--- 04week/loop.js
+++ 04week/loop.js
@@ -6,3 +6,21 @@ const arrLength = carsInReverse.length;
 for(i = 0; i < arrLength; i++){
   console.log (carsInReverse[i]);
 };
+
+//Create Object, Use For Loop to go through object and return value for key: birthDate
+const persons =
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    birthDate: "Jan 5, 1925",
+    gender: "female",
+  };
+
+
+const keys = Object.keys(persons);
+
+for(i = 0; i < keys.length; i++){
+  if(i == keys.indexOf('birthDate')){
+    console.log(persons.birthDate);
+  }
+};
