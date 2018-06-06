74423f3dd7b05155a72f43f452505f4190669b1f
Cameron Ornelas <35507124+CameronOrnelas@users.noreply.github.com>
2018-06-05 19:05:34 -0500

    Changes

--- 07week/todolist/src/App.js
+++ 07week/todolist/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      userHasClicked: 0,
+    }
+  }
+  handleButtonClick=()=>{
+    console.log('click');
+    this.setState({userHasClicked: this.state.userHasClicked +1})
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +20,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Texas</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro" style={{backgroundColor: 'lightblue'}}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleButtonClick}>Click Me!</button>
+        {this.state.userHasClicked}
       </div>
     );
   }
