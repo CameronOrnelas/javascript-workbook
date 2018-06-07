3ef2a9ce272e0763f0d1cd0cc7e5963fa6b7d57e
Cameron Ornelas <35507124+CameronOrnelas@users.noreply.github.com>
2018-06-07 15:27:05 -0500

    Vertical Win

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -38,7 +38,15 @@ function horizontalWin() {
 };
 
 function verticalWin() {
-  // Your code here
+  if(board[0][0] == playerTurn && board [1][0] == playerTurn && board [2][0] == playerTurn){
+    return true;
+  } else if(board[0][2] == playerTurn && board [1][2] == playerTurn && board [2][2] == playerTurn){
+    return true;
+  } else if(board[0][1] == playerTurn && board [1][1] == playerTurn && board [2][1] == playerTurn){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 function diagonalWin() {
